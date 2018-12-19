d45819c30d978ee3932ea0fc596de63f67d4fa0a
Benjamin Gray <bengray178@gmail.com>
2018-12-19 23:19:11 +1000

    Working communication

--- src/message_handler.cpp
+++ src/message_handler.cpp
@@ -6,6 +6,7 @@
 #include "communications.h"
 
 #define ASSERT_PROP(json, prop, val) if (!json.HasMember(prop) || std::string(json[prop].GetString()) != val) exit(1);
+#define ASSERT_EXISTS(json, prop) if (!json.HasMember(prop)) exit(2);
 
 void assertJsonValid (Document &json) {
     ASSERT_PROP(json, "jsonrpc", "2.0")
@@ -15,19 +16,27 @@ void assertJsonValid (Document &json) {
 
 void handleMessage (Document &json) {
     assertJsonValid(json);
+
+
+    // Just echo it back right now
+    sendMessage(json);
 }
 
 void handleInitRequest (Document &json) {
     assertJsonValid(json);
 
     ASSERT_PROP(json, "method", "initialize");
+    ASSERT_EXISTS(json, "id");
+
+    int id = json["id"].GetInt();
 
     std::string content =
         "{"
-            "\"id\":1,"
+            "\"id\":\"" + std::to_string(id) + "\","
             "\"result\":{"
                 "\"capabilities\":{"
-                    "\"textDocumentSync\":2"
+                    "\"textDocumentSync\":2,"
+                    "\"hoverProvider\":true"
                 "}"
             "}"
         "}";

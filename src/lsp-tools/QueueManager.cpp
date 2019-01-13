a165ff5197515231b261fc5b29fe89b301005bb0
Aerijo <bengray178@gmail.com>
2019-01-13 23:35:11 +1000

    clean

--- src/lsp-tools/QueueManager.cpp
+++ src/lsp-tools/QueueManager.cpp
@@ -9,17 +9,10 @@ void QueueManager::init () {
 }
 
 void QueueManager::writeStdout (Document &message) {
-//    std::cerr << "writing message to stdout...\n";
-//    sendMessage(message);
-
-//    if (!message.IsObject()) {
-//        std::cerr << "NULL ON THE WAY OUT BOIS!!!\n";
-//    }
     getInstance()->for_stdout.enqueue(std::move(message));
 }
 
 void QueueManager::pushMessage (Document &message) {
-    std::cerr << "pushing message for handlers...\n";
     getInstance()->for_handlers.enqueue(std::move(message));
 }
 

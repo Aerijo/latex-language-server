11ac700b36216b37f32d89b5fc4e613b1fcdc99e
Aerijo <bengray178@gmail.com>
2019-01-19 12:04:46 +1000

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <string>
+
+#include <locale>
+
+//#include <utf8-conversions.h>
 
 #include <tree_sitter/runtime.h>
 #include <filesystem/Uri.h>
@@ -12,24 +17,40 @@
 
 #include "text-buffer.h"
 
+#include <encoding-conversion.h>
+
 extern "C" { TSLanguage *tree_sitter_biber(); }
+
+using std::u16string;
 
 GlobalConfig *g_config;
 
 int main (int argc, char** argv, const char** env) {
     std::cerr << "Running latex-language-server...\n";
 
-    Uri foo = Uri::parse("file:///home/benjamin/test/exp.tex");
-
-    foo.print();
-
     g_config = new GlobalConfig();
 
     TextBuffer buffer {};
 
-//    buffer.find_words_with_subsequence_in_range({ "foo" }, "bar", { { 0,0}, {3,4}});
+    auto conversion = transcoding_to("UTF-8");
+    u16string string = u"ab" "\xd83d" "\xde01" "cd";  // 'ab😁cd'
 
-//    Text bazx = buffer
+    size_t foo = 10;
+
+    std::string output;
+
+    std::cout << string.length() << std::endl;
+
+    output.resize(foo);
+
+    size_t bytes_encoded = 0, start = 0;
+
+    bytes_encoded = conversion->encode(
+            string, &start, string.size(), output.data(), output.size());
+
+    output.resize(bytes_encoded);
+
+    std::cout << output << std::endl;
 
     TSParser *parser = ts_parser_new();
 
